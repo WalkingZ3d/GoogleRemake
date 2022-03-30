b4d0f83bc88478849fc0087c1017261dfc2f0427
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-03-30 17:05:22 +0100

    sports

--- server/app.js
+++ server/app.js
@@ -15,25 +15,73 @@ const newsPages = [
 
 JSON.stringify(newsPages);
 
+const sportsPages = [
+    {name: 'bbc', link: "https://www.bbc.co.uk/news"},
+    {name: "sky", link: "https://news.sky.com/uk"},
+    {name: "itv", link: "https://www.itv.com/news"},
+    {name: "channel 4", link: "https://www.channel4.com/news/"},
+    {name: "guardian", link: "https://www.theguardian.com/tone/news"},
+    {name: "telegraph", link: "https://www.telegraph.co.uk/news/"},
+    {name: "independent", link: "https://www.independent.co.uk/"},
+    {name: "evening standard", link: "https://www.standard.co.uk/news"},
+]
+
+JSON.stringify(sportsPages);
+
 app.use(cors());
 
-const errorMsg = {"error: ": "Invalid news site name"}
+const errorMsg = {"error: ": "Invalid site name"}
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.send('Hello World!')
+    try {
+        res.statusCode = 200;
+        res.send('Our Version of Google is Better!')
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }
+  
 })
 
 app.get('/newsPages', (req, res) => {
-  res.statusCode = 200;
-  res.send(newsPages);
+    try {
+        res.statusCode = 200;
+        res.send(newsPages);
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }  
 });
 
 app.get('/newsPages/news', (req, res) => {
-    res.statusCode = 200;
-    res.send(newsPages);
+    try {
+        res.statusCode = 200;
+        res.send(newsPages);
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }  
 });
 
 
+app.get('/sportsPages', (req, res) => {
+    try {
+        res.statusCode = 200;
+        res.send(sportsPages);
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }  
+});
+
+app.get('/sportsPages/sports', (req, res) => {
+    try {
+        res.statusCode = 200;
+        res.send(sportsPages);
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }  
+});
 
 module.exports = app
